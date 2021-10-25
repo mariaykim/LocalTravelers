a366bca0c8fd1a9a0ea760c66dffb779e6775a93
mariaykim <jsmkim09@gmail.com>
2021-10-25 17:26:34 -0500

    Installation of prisma

--- components/layout.js
+++ components/layout.js
@@ -4,23 +4,16 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Gouda'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Maria Kim'
+export const siteTitle = 'LocalTravelers website'
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="Travel like a local would"
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
